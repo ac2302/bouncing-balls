4584b77638307a5bfff835c9b22ffa9dc1897439
Aryan Chavan <aryan@aryansdomain.com>
2021-06-26 20:00:06 +0530

    fix typo in generate-video.sh

--- generate-video.sh
+++ generate-video.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ffmpeg -r 30 -s 1920x1080 -i taf/frames/%06d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p bounce.mp4
-ffmpef -i bounce.mp4 bounce.gif
+ffmpeg -i bounce.mp4 bounce.gif
 
